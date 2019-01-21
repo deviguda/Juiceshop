401797a42db69d0a2440462b3f91dccf23684718
deviguda <45884882+deviguda@users.noreply.github.com>
2019-01-21 15:18:46 -0600

    Update xrdp_install.sh

--- xrdp_install.sh
+++ xrdp_install.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-sudo apt-get update
-sudo apt-get install lxde 
-sudo apt-get install xrdp
-sudo /etc/init.d/xrdp start
+sudo apt-get update && apt-get install lxde xrdp -y && /etc/init.d/xrdp start 
